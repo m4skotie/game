b64ba0f74896c9b7b309022c7a37b90b62af46f4
m4skotie <126868115+m4skotie@users.noreply.github.com>
2025-12-16 12:29:56 +0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,48 +1,53 @@
 export class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = 32;
-        this.velX = 0;
-        this.velY = 0;
-        this.speed = 4;
-        this.jump = -12;
-        this.gravity = 0.6;
-        this.onGround = false;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 30;
+    this.h = 30;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 5;
+    this.jumpPower = 14;
+    this.gravity = 0.6;
+    this.onGround = false;
+  }
+
+  update(input) {
+    this.vx = 0;
+
+    if (input.keys['ArrowLeft'] || input.keys['a'] || input.keys['A']) {
+      this.vx = -this.speed;
+    }
+    if (input.keys['ArrowRight'] || input.keys['d'] || input.keys['D']) {
+      this.vx = this.speed;
     }
 
-    update(input) {
-        if (input.left) this.velX = -this.speed;
-        else if (input.right) this.velX = this.speed;
-        else this.velX = 0;
-
-        if (input.jump && this.onGround) {
-            this.velY = this.jump;
-            this.onGround = false;
-        }
-
-        this.velY += this.gravity;
-        this.x += this.velX;
-        this.y += this.velY;
+    if ((input.keys[' '] || input.keys['ArrowUp'] || input.keys['w'] || input.keys['W']) && this.onGround) {
+      this.vy = -this.jumpPower;
+      this.onGround = false;
     }
 
-    collides(p) {
-        return (
-            this.x < p.x + p.width &&
-            this.x + this.size > p.x &&
-            this.y + this.size > p.y &&
-            this.y < p.y + p.height
-        );
-    }
+    this.vy += this.gravity;
+    this.x += this.vx;
+    this.y += this.vy;
 
-    landOn(p) {
-        this.y = p.y - this.size;
-        this.velY = 0;
-        this.onGround = true;
-    }
+    // Границы
+    if (this.x < 0) this.x = 0;
+    if (this.x + this.w > 800) this.x = 800 - this.w;
+  }
 
-    draw(ctx) {
-        ctx.font = "32px serif";
-        ctx.fillText("😀", this.x, this.y + 28);
-    }
+  checkCollision(obj) {
+    return (
+      this.x < obj.x + obj.w &&
+      this.x + this.w > obj.x &&
+      this.y < obj.y + obj.h &&
+      this.y + this.h > obj.y
+    );
+  }
+
+  draw(ctx) {
+    ctx.font = '30px Arial';
+    ctx.textAlign = 'left';
+    ctx.fillText('😄', this.x, this.y + this.h);
+  }
 }
