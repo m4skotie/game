32b72e9a90c30695b254229f84a889f528ce9011
m4skotie <126868115+m4skotie@users.noreply.github.com>
2025-12-16 13:05:48 +0300

    Update Item.js

--- js/Item.js
+++ js/Item.js
@@ -2,8 +2,15 @@ export class Item {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.baseY = y;
     this.w = 24;
     this.h = 24;
+    this.time = Math.random() * Math.PI * 2;
+  }
+
+  update() {
+    this.time += 0.05;
+    this.y = this.baseY + Math.sin(this.time) * 4;
   }
 
   draw(ctx) {
