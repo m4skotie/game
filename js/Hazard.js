bff99d9f969e376c9838129b51e03183022720cf
m4skotie <126868115+m4skotie@users.noreply.github.com>
2025-12-16 12:47:12 +0300

    Create Hazard.js

---
+++ js/Hazard.js
@@ -0,0 +1,17 @@
+export class Hazard {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  draw(ctx) {
+    ctx.font = '24px Arial';
+    ctx.textAlign = 'center';
+    // Рисуем огонь по всей ширине с шагом
+    for (let i = this.x + 12; i < this.x + this.w; i += 24) {
+      ctx.fillText('🔥', i, this.y + 20);
+    }
+  }
+}
